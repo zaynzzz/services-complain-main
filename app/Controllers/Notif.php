c4f35547abb3e0fca40f7873f01f8748cca87bda
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2022-04-27 13:32:30 +0700

    versi.1.0.1

--- app/Controllers/Notif.php
+++ app/Controllers/Notif.php
@@ -6,74 +6,28 @@ use App\Controllers\BaseController;
 
 class Notif extends BaseController
 {
+    public function showmess()
+    {
+        $sql = db_connect()->query("SELECT * from comments ")->getResult();
+        while ($sql) {
+            $sql;
+        }
+        echo json_encode($sql);
+    }
+    public function store()
+    {
+        $subject = $this->request->getVar('subject');
+        $comment = $this->request->getVar('comment');
+        $query = "INSERT INTO comments(comment_subject, comment_text)VALUES ('$subject', '$comment')";
+    }
+    public function jumlah()
+    {
+        $sql = db_connect()->query("SELECT * from comments")->getNumRows();
+        return json_encode($sql);
+    }
     public function index()
     {
-        //
-    }
-    public function sendPushNotification()
-    {
-
-        $val = $this->validate([
-            'nId' => 'required',
-        ]);
-
-        $notification_id = $this->request->getVar('nId');
-        $title = 'Demo Notification';
-        $message = 'First codeigniter notification for mobile';
-        $d_type    = $this->request->getVar('device_type');  // for android or IOS
-
-        $accesstoken = 'YOUR FCM KEY';
-
-        $URL = 'https://fcm.googleapis.com/fcm/send';
-
-
-        $post_data = '{
-                "to" : "' . $notification_id . '",
-                "data" : {
-                  "body" : "",
-                  "title" : "' . $title . '",
-                  "type" : "' . $d_type . '",
-                  "id" : "' . $id . '",
-                  "message" : "' . $message . '",
-                },
-                "notification" : {
-                     "body" : "' . $message . '",
-                     "title" : "' . $title . '",
-                      "type" : "' . $d_type . '",
-                     "id" : "' . $id . '",
-                     "message" : "' . $message . '",
-                    "icon" : "new",
-                    "sound" : "default"
-                    },
- 
-              }';
-        // print_r($post_data);die;
-
-        $crl = curl_init();
-
-        $headr = array();
-        $headr[] = 'Content-type: application/json';
-        $headr[] = 'Authorization: ' . $accesstoken;
-        curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, false);
-
-        curl_setopt($crl, CURLOPT_URL, $URL);
-        curl_setopt($crl, CURLOPT_HTTPHEADER, $headr);
-
-        curl_setopt($crl, CURLOPT_POST, true);
-        curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
-        curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
-
-        $rest = curl_exec($crl);
-
-        if ($rest === false) {
-            // throw new Exception('Curl error: ' . curl_error($crl));
-            //print_r('Curl error: ' . curl_error($crl));
-            $result_noti = 0;
-        } else {
-
-            $result_noti = 1;
-        }
-
-        echo view('success');
+        $data['contents'] = "Notif/index";
+        return view('Layout/template', $data);
     }
 }
